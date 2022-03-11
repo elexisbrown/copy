8f02114ff357b21cb0ac8d1cecb341847df511db
elexis <eebrown422@gmail.com>
2022-03-11 00:04:46 -0500

    tried to add general formatting of home page, not final look of course

---
+++ Home.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+
+    
+    <div>
+
+
+    <div  className='center' id='hero-image'> 
+      <img src='https://via.placeholder.com/1500x400' />
+      </div>
+
+      <h4 className="center">HISTORY OF CINCINNATI</h4>
+ 
+      <div class="container">
+    <div class="image">
+      <img class="small-image" src="https://via.placeholder.com/400"/>
+    </div>
+    <div class="a-paragraph">
+      <h3>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quisquam provident praesentium, illum saepe ea odio at adipisci similique ipsum, omnis, ipsam iste odit repellat nostrum.</h3>
+    </div>
+  </div>
+
+         <div className='center'> 
+            <h3> Lorem ipsum dolor sit amet consectetur adipisicing elit.</h3>
+         </div>
+         <div>  
+            <h1 className='center' >Attractions</h1>
+         </div>
+    </div>
+    
+  )
+}
+
+export default Home
