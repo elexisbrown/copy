d75e5adf72b359a6461320e9ce4fa5f730cdd305
elexis <eebrown422@gmail.com>
2022-03-21 23:53:13 -0400

    added images + link to cards

--- Home.js
+++ Home.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import heroVideo from '../videos/cincinnati.mp4';
+import heroVideo from '../videos/cincinnati.mp4'
 
 
 const Home = () => {
@@ -11,19 +11,8 @@ const Home = () => {
 
 
     <div  className='center'> 
-    <header>
-        <section className="hero-image-box">
-        
-          <video src={heroVideo} className="hero-video" autoplay muted loop typeof='video/mp4'>
-            <source />
-          </video>
-        </section>
-        <section className="hero-text-box">
-          <h1 className="hero-heading">Placeholder</h1>
-          <p className="hero-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, fuga?</p>
-        </section>
-    </header>
-      <img id='hero-image' src='https://via.placeholder.com/1500x400' />
+
+      <img id='hero-image' src='https://upload.wikimedia.org/wikipedia/commons/thumb/6/6d/Downtown_Cincinnati_viewed_from_Devou_Park_Cropped.jpg/1000px-Downtown_Cincinnati_viewed_from_Devou_Park_Cropped.jpg' />
       </div>
 
       <h4 id='title-padding' className="center">HISTORY OF CINCINNATI</h4>
@@ -50,30 +39,51 @@ const Home = () => {
          </div>
          <div className='card-group'>
          <div className="card">
-           <img className='card-img-top' src="https://via.placeholder.com/400x200" alt="Card image cap" />
+           <a href="/food">
+           <img className='card-img-top' src="https://cincinnatiusa.com/sites/default/files/styles/Array/public/Venues/56_LaRosa%27s%20Pizza_DSC_0579.JPG" alt="Card image cap" />
+           </a>
            <div className="card-body">
-             <h5 className="card-title">Card Title</h5>
+             <h5 className="card-title">Food & Drink</h5>
              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
            </div>
          </div>
          <div className="card">
-           <img className='card-img-top' src="https://via.placeholder.com/400x200" alt="Card image cap" />
+         <a href="/Arts">
+
+           <img className='card-img-top' src="https://www.cincymuseum.org/wp-content/uploads/2020/05/UT-1.jpg" alt="Card image cap" />
+           </a>
            <div className="card-body">
-             <h5 className="card-title">Card Title</h5>
+             <h5 className="card-title">Arts & Music</h5>
              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
            </div>
          </div>
          <div className="card">
-           <img className='card-img-top' src="https://via.placeholder.com/400x200" alt="Card image cap" />
+         <a href="/Sports">
+
+           <img className='card-img-top' src="https://d3pxppq3195xue.cloudfront.net/media/images/13/01/25/Paul-BrownExterior_966x668.jpg" alt="Card image cap" />
+           </a>
            <div className="card-body">
-             <h5 className="card-title">Card Title</h5>
+             <h5 className="card-title ">Sports</h5>
              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
            </div>
          </div>
          <div className="card">
-           <img className='card-img-top' src="https://via.placeholder.com/400x200" alt="Card image cap" />
+         <a href="/theme">
+
+           <img className='card-img-top' src="https://d31029zd06w0t6.cloudfront.net/wp-content/uploads/sites/28/2020/06/Kings-Island.jpg" alt="Card image cap" />
+           </a>
            <div className="card-body">
-             <h5 className="card-title">Card Title</h5>
+             <h5 className="card-title">Amusement Parks</h5>
+             <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+           </div>
+         </div>
+         <div className="card">
+         <a href="/night">
+
+           <img className='card-img-top' src="https://www.gannett-cdn.com/presto/2019/10/11/PCIN/93a801a0-6279-4056-8994-581d7d6a444e-BLINK2019_MV_0001.JPG?crop=3899,2193,x1,y0&width=660&height=372&format=pjpg&auto=webp" alt="Card image cap" />
+           </a>
+           <div className="card-body">
+             <h5 className="card-title">Night Life</h5>
              <p className="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
            </div>
          </div>
